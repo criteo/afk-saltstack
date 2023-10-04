c960d4432772b1aa0282fd5f2691a1fd64dbdc82
Kevin Petremann <k.petremann@criteo.com>
2023-10-04 16:42:16 +0200

    test: template mock is now using salt function
    To be closer to Salt behaviour.
    Without it, some errors were not reported as default jinja configuration
    seems to be more permissive.

--- tests/common.py
+++ tests/common.py
@@ -1,12 +1,28 @@
 from jinja2 import BaseLoader, Environment
 
 
-def mock_apply_template_on_contents(contents, template, context, *_, **__):
-    """Mock apply_template_on_contents with real jinja."""
-    assert template == "jinja"
-    loader = Environment(loader=BaseLoader)
-    template = loader.from_string(contents)
-    return template.render(**context)
+def mock_apply_template_on_contents(contents, template, context, defaults, saltenv):
+    """Remove salt:// prefix in path file."""
+    context_dict = defaults if defaults else {}
+    if context:
+        context_dict.update(context)
+    # Apply templating
+    res = saltTemplates.TEMPLATE_REGISTRY[template](
+        contents,
+        from_str=True,
+        to_str=True,
+        context=context_dict,
+        saltenv=None,
+        grains={},
+        pillar={},
+        salt={},
+        opts={},
+    )["data"]
+    if isinstance(res, bytes):
+        # bytes -> str
+        res = res.decode("utf-8")
+
+    return res
 
 
 def mock_get_file_str(template_name, *_, **__):
