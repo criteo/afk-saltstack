685b6ecf15b8ebd96b1981b4eaeacdd5b6d87d1c
Kevin Petremann <k.petremann@criteo.com>
2024-04-09 11:57:58 +0200

    fix: net.cli does not set retcode in context

--- _states/afk_bgp.py
+++ _states/afk_bgp.py
@@ -16,16 +16,18 @@ def clear_soft_all(name):
         time.sleep(5)  # we wait for bgp route-map delay-timer (which is set to 5 seconds)
         res = __salt__["cmd.run"]("vtysh -c 'clear bgp * soft'")
         ret["changes"]["executed"] = "vtysh -c 'clear bgp * soft"
+        ret["result"] = __context__["retcode"] == 0
     elif nos == "junos":
         res = __salt__["net.cli"]("clear bgp neighbor soft all")
         ret["changes"]["executed"] = "clear bgp neighbor soft all"
+        ret["result"] = res["result"]
     elif nos == "eos":
         res = __salt__["net.cli"]("clear bgp soft")
         ret["changes"]["executed"] = "clear bgp soft"
+        ret["result"] = res["result"]
     else:
         raise NotImplementedError("Network OS not supported")
 
-    ret["result"] = __context__["retcode"] == 0
     ret["comment"] = res
 
     return ret
